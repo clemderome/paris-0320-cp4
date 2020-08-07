33ec93800b84aa67207041d99926132313727837
clemderome <clementine.derome@gmail.com>
2020-08-07 13:02:34 +0200

    clean code

--- back/routes/index.js
+++ back/routes/index.js
@@ -3,7 +3,6 @@ const router = express.Router()
 
 const connection = require('../helper/db')
 
-// Chercher tous les cadeaux dans la BDD et les renvoyer en JSON
 router.get('/gifts', (req, res) => {
   const sql = 'SELECT * FROM gift'
   connection.query(sql, (err, result) => {
@@ -11,7 +10,7 @@ router.get('/gifts', (req, res) => {
     return res.status(200).send(result)
   })
 })
-// Ajouter la route POST avec le chemin /gifts
+
 router.post('/gifts', (req, res) => {
   const formData = req.body
   connection.query('INSERT INTO gift SET ?', formData, (err, result) => {
@@ -22,7 +21,17 @@ router.post('/gifts', (req, res) => {
     }
   })
 })
-// Ajouter la route DELETE avec le chemin /gifts/:giftId
-// (giftId = l'id du cadeau à supprimer)
+
+router.delete('/gifts/:id', (req, res) => {
+  const giftId = req.params.id
+  console.log(giftId)
+  connection.query('DELETE FROM gift WHERE id = ?', [giftId], err => {
+    if (err) {
+      res.status(500).send('Erreur lors de la suppression d\'un cadeau')
+    } else {
+      res.status(200).send('Le cadeau a bien été supprimé')
+    }
+  })
+})
 
 module.exports = router
